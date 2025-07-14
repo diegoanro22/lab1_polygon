7285b954fd91d50b740f822424aaaf783c9deff9
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-07-13 20:57:02 -0600

    feat: :sparkles: create Polygon3

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,28 @@ use polygon::fill_polygon;
 fn main() {
     let width = 800;
     let height = 600;
-    let background_color = Color::new(50, 50, 100, 255);
+    let background_color = Color::BLACK;
     let mut framebuffer = FrameBuffer::new(width,height, background_color);
 
-    framebuffer.clear();
+
+    let polygon3 = vec![
+        Vector2::new(377.0,249.0),
+        Vector2::new(411.0,197.0),
+        Vector2::new(436.0, 249.0),
+    ];
+
+    framebuffer.set_color(Color::RED);
+    fill_polygon(&mut framebuffer, &polygon3);
+
+    framebuffer.set_color(Color::WHITE);
+    for i in 0..polygon3.len() {
+        let start = polygon3[i];
+        let end = polygon3[(i + 1) % polygon3.len()];
+        line(&mut framebuffer, start, end);
+    }
 
 
-
-    let output_file = "out.bmp";
+    let output_file = "out.png";
     framebuffer.render_to_file(output_file)
         .expect("Failed to save framebuffer to file");
 }
